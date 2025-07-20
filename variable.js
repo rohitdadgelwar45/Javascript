1d1c95207e49691d757e8ba558b4852aff319350
rohit <rohitdadgelwar123@gmail.com>
2025-07-20 16:32:34 +0530

    adding note

--- variable.js
+++ variable.js
@@ -5,6 +5,8 @@ accountState="Maharashtra"
 
 // console.log(accountId)
 // accountId=20220
+
+//prefered not used to var because of block or functional scope
 accountName="Tushar"
 accountCity="Nagpur"
 console.table([accountId,accountName,accountCity,accountState])
