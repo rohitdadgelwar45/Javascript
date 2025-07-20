be68c5ee263ff5026b06d1ed2edab9ead096cb3b
rohit <rohitdadgelwar123@gmail.com>
2025-07-20 16:30:07 +0530

    adding variable.js file

---
+++ variable.js
@@ -0,0 +1,10 @@
+const accountId=112
+let accountName="Rohit"
+var accountCity="Pune"
+accountState="Maharashtra"
+
+// console.log(accountId)
+// accountId=20220
+accountName="Tushar"
+accountCity="Nagpur"
+console.table([accountId,accountName,accountCity,accountState])
