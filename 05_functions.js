7d297b197d091aac467edea13de6dbcd3705bcef
rohit <rohitdadgelwar123@gmail.com>
2025-07-23 19:19:23 +0530

    adding function  file

---
+++ 05_functions.js
@@ -0,0 +1,16 @@
+// function initialization and declaration
+// function with no parameter
+function helloWorld(){
+    console.log("Hello World!");
+}
+
+// function with parameter 
+function add(num1,num2){
+    return num1+num2;
+}
+
+// calling a function
+helloWorld();
+let rohit=add(50,100);
+console.log(rohit);
+console.log(add(10,20));
