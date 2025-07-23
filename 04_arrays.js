9699e919f995691e0adafdff8bda39d5224a5302
rohit <rohitdadgelwar123@gmail.com>
2025-07-23 19:06:24 +0530

    adding array file

---
+++ 04_arrays.js
@@ -0,0 +1,20 @@
+let cars=["volvo", "BMW", "Tata", "Mahindra"];// storing same type of data
+let Rohit=["Rohit", 22, 8010798482, "Pune"]; // storing different types of data
+
+// accessing whole array 
+console.log(cars);
+
+// accessing element of an array 
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+console.log(cars[3]);
+
+// adding element in array at last
+cars.push("Honda");
+console.log(cars);
+console.log(Rohit);
+
+// removing last element of an array 
+Rohit.pop();
+console.log(Rohit);
